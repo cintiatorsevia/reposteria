020b474feed30e7da09ab5acfcf1dd3eef49c191
cintiatorsevia <cintiatorsevia@gmail.com>
2020-07-30 02:17:37 -0300

    Updates

---
+++ precache-manifest.e62948467617f2bc6a771a4ad02ad54f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f4057f2e993406449f1d10a995489f",
+    "url": "/reposteria/index.html"
+  },
+  {
+    "revision": "5573dbea0ac9b01b74ca",
+    "url": "/reposteria/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7f9fa7fae1ec7d7b3171",
+    "url": "/reposteria/static/css/main.3d9f13ac.chunk.css"
+  },
+  {
+    "revision": "5573dbea0ac9b01b74ca",
+    "url": "/reposteria/static/js/2.bd000b3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reposteria/static/js/2.bd000b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f9fa7fae1ec7d7b3171",
+    "url": "/reposteria/static/js/main.3340f374.chunk.js"
+  },
+  {
+    "revision": "25495b5f4954b39949f0",
+    "url": "/reposteria/static/js/runtime-main.bba4d52c.js"
+  },
+  {
+    "revision": "2ac43ba3be392f0c76714c63574a069b",
+    "url": "/reposteria/static/media/1.2ac43ba3.jpeg"
+  },
+  {
+    "revision": "190b7ad3e9803d22433b0ddf88cd2bde",
+    "url": "/reposteria/static/media/11.190b7ad3.jpeg"
+  },
+  {
+    "revision": "4d798920ec98b2907e6b17f0a53545d9",
+    "url": "/reposteria/static/media/12.4d798920.jpeg"
+  },
+  {
+    "revision": "95e5d410e7e60dbf5ab2b3082c6e6926",
+    "url": "/reposteria/static/media/13.95e5d410.jpeg"
+  },
+  {
+    "revision": "bc9e50c2362b1ede32caffe03da03983",
+    "url": "/reposteria/static/media/14.bc9e50c2.jpeg"
+  },
+  {
+    "revision": "0c8f977bdd99b7a7166a9410bcc2dff3",
+    "url": "/reposteria/static/media/15.0c8f977b.jpeg"
+  },
+  {
+    "revision": "50fd7e29658798692cf8aec161162e70",
+    "url": "/reposteria/static/media/16.50fd7e29.jpeg"
+  },
+  {
+    "revision": "863d735e50f4cc5263132e8f9c1b3017",
+    "url": "/reposteria/static/media/17.863d735e.jpeg"
+  },
+  {
+    "revision": "3a77875993d2b98309bcd6fee042842f",
+    "url": "/reposteria/static/media/19.3a778759.jpeg"
+  },
+  {
+    "revision": "7d83e85cf2fe31677e300059579e6a99",
+    "url": "/reposteria/static/media/2.7d83e85c.jpeg"
+  },
+  {
+    "revision": "2feb515eb19304b86af1addc80b8160b",
+    "url": "/reposteria/static/media/20.2feb515e.jpeg"
+  },
+  {
+    "revision": "4934c75acbec37424d10c2e5017bb517",
+    "url": "/reposteria/static/media/21.4934c75a.jpeg"
+  },
+  {
+    "revision": "432beca41df5df68bede4d95e215403f",
+    "url": "/reposteria/static/media/22.432beca4.jpeg"
+  },
+  {
+    "revision": "25c886c764c2dfe2fcf947c11a09813a",
+    "url": "/reposteria/static/media/3.25c886c7.jpeg"
+  },
+  {
+    "revision": "d191402c723f62b7a3f001140ed7ab95",
+    "url": "/reposteria/static/media/4.d191402c.jpeg"
+  },
+  {
+    "revision": "164b9fe17118534a3131b68f6af5b5fb",
+    "url": "/reposteria/static/media/5.164b9fe1.jpeg"
+  },
+  {
+    "revision": "2ffeed86c1929b302bb50914a9918bbb",
+    "url": "/reposteria/static/media/6.2ffeed86.jpeg"
+  },
+  {
+    "revision": "6f4adf4232eb467613fa409c9899b44a",
+    "url": "/reposteria/static/media/7.6f4adf42.jpeg"
+  },
+  {
+    "revision": "5167a310c169d738561941a3b26f7b97",
+    "url": "/reposteria/static/media/8.5167a310.jpeg"
+  },
+  {
+    "revision": "08e081b707dd20e3a3d86df4ad506df5",
+    "url": "/reposteria/static/media/9.08e081b7.jpeg"
+  },
+  {
+    "revision": "a8f9a72f7b95e074288ce821754f63de",
+    "url": "/reposteria/static/media/background.a8f9a72f.png"
+  },
+  {
+    "revision": "67be9120c290a832ac3911b1d6744369",
+    "url": "/reposteria/static/media/insta.67be9120.png"
+  }
+]);
